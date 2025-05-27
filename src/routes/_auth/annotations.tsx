4c7a80786ec5e29153a1de6d19c17723e93bbd9e
Simone Baffelli <simone.baffelli@empa.ch>
2025-05-27 15:15:52 +0200

    Deleted unused Annotations component

--- src/routes/_auth/annotations.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import Annotations from '../../components/type/Annotations'
-
-export const Route = createFileRoute('/_auth/annotations')({
-  component: () => <Annotations />,
-})
